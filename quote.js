17f01ca45f7c3a57e59b062bcf16024760cac53d
romorin <romorin@zoho.com>
2016-04-18 18:38:02 -0400

    forgot quote.js in last commit

--- quote.js
+++ quote.js
@@ -1,9 +1,22 @@
-var getNumberJsonFct = getTestNumberJson; // getTestNumberJson or getNumberJson
+var getNumberJsonFct = getNumberJson; // getTestNumberJson or getNumberJson
+
+var BASE_TWITTER = "https://twitter.com/intent/tweet?text=";
 
 // format the page from the received json
 function loadFromNumberJson(json) {
+	// update html
 	jQuery('#number').html(json.number);
 	jQuery('#contents').html(json.text.replace(json.number, '...'));
+
+	// update text to be sent over twitter
+	var twitterShareText;
+
+	if (json.text.length > 137) {
+		twitterShareText = json.text.slice(0, 137) + "...";
+	} else {
+		twitterShareText = json.text;
+	}
+	jQuery('#twitter').attr("href", BASE_TWITTER + encodeURIComponent(twitterShareText));
 }
 
 // get number data for testing without hitting the api
@@ -25,4 +38,4 @@ function getNumberJson() {
 jQuery(document).ready(function() {
   	jQuery("#next").on("click", getNumberJsonFct);
 	getNumberJsonFct();
-}); 
+});
