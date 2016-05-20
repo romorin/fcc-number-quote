5d2b86a5e40187e927b176d1e1e4e10dbffba347
romorin <romorin@zoho.com>
2016-05-20 11:58:59 -0400

    1 - Oops, forgot to add some files in last commit 2 - some minor cleanup

--- quote.js
+++ quote.js
@@ -1,41 +1,41 @@
-var getNumberJsonFct = getTestNumberJson; // getTestNumberJson or getNumberJson
+var getNumberJsonFct = getNumberJson; // getTestNumberJson or getNumberJson
 
 var BASE_TWITTER = "https://twitter.com/intent/tweet?text=";
 
 // format the page from the received json
 function loadFromNumberJson(json) {
-	// update html
-	jQuery('#number').html(json.number);
-	jQuery('#contents').html(json.text.replace(json.number, '...'));
+   // update html
+   jQuery('#number').html(json.number);
+   jQuery('#contents').html(json.text.replace(json.number, '...'));
 
-	// update text to be sent over twitter
-	var twitterShareText;
+   // update text to be sent over twitter
+   var twitterShareText;
 
-	if (json.text.length > 137) {
-		twitterShareText = json.text.slice(0, 137) + "...";
-	} else {
-		twitterShareText = json.text;
-	}
-	jQuery('#twitter').attr("href", BASE_TWITTER + encodeURIComponent(twitterShareText));
+   if (json.text.length > 137) {
+      twitterShareText = json.text.slice(0, 137) + "...";
+   } else {
+      twitterShareText = json.text;
+   }
+   jQuery('#twitter').attr("href", BASE_TWITTER + encodeURIComponent(twitterShareText));
 }
 
 // get number data for testing without hitting the api
 function getTestNumberJson() {
-	return loadFromNumberJson({
-		"text": "15250 is the number of times the average driver will honk in a lifetime.",
-		"number": 15250,
-		"found": true,
-		"type": "trivia"
-	});
+   return loadFromNumberJson({
+      "text": "15250 is the number of times the average driver will honk in a lifetime.",
+      "number": 15250,
+      "found": true,
+      "type": "trivia"
+   });
 }
 
 // get number data from the api
 function getNumberJson() {
-	return jQuery.getJSON("http://numbersapi.com/random/trivia?json", loadFromNumberJson);
+   return jQuery.getJSON("http://numbersapi.com/random/trivia?json", loadFromNumberJson);
 }
 
 // when the page is ready
 jQuery(document).ready(function() {
-  	jQuery("#next").on("click", getNumberJsonFct);
-	getNumberJsonFct();
+   jQuery("#next").on("click", getNumberJsonFct);
+   getNumberJsonFct();
 });
